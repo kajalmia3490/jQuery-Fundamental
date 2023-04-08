0605170c20a9004ea9e71de0f5c586306bb5786a
kajalmia3490 <kajalmia3490@gmail.com>
2023-04-09 02:48:50 +0600

    'message'

---
+++ jQuery/app.js
@@ -0,0 +1,46 @@
+// // jQuery usecase:
+// $('.button').click(function () {
+//     $('ul').text($('input').val())
+
+//     let li = $('<li>').html($('input').val())
+
+//     let ul = $('ul')
+//     let li1 = $('<li>').html('input').val()
+//     let li2 = $('<li>').html($('input').val())
+
+//     ul.append(li, li1, li2)
+// })
+
+// // trial style and events
+// $('p').css('color', 'blue')
+
+// $('.myBtn').click(function () {
+//     $('p').hide($('p').text('hidden your Questions!').css('color', 'black'))
+
+// })
+
+// // password checkout
+// let pass = $('.buttons').click(function () {
+//     if ($('.input1').val() === $('.input2').val()) {
+//         alert('Successfully logged in')
+//     } else if ($('.input1').val() === '' && $('.input2').val() === '') {
+//         alert('Type your password!')
+//     } else if ($('input1').val() != '') {
+//         alert('Try again!')
+//     } else {
+//         alert('Wrong password!')
+//     }
+// })
+
+// // animation (fadeIn-visible, fadeOut-invisible, fadeTo, slideUp, slideDown, slideToggle)
+// $('.buttons2').click(function () {
+//     $('.p').slideToggle(2000)
+//     $('.buttons2').css('margin-top', '30px')
+// })
+
+// // animate
+// $('.buttons3').click(function () {
+//     $('.p-next').animate({
+//         paddingBottom: '100px'
+//     }, 1000)
+// })
